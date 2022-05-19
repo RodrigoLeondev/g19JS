c5fba12bac876724d02b4cbe4257ee214ea4f1d6
RodrigoLeondev <rodrigoleon.dev@gmail.com>
2022-05-18 22:14:36 -0500

    add 2

---
+++ is_largest.js
@@ -0,0 +1,20 @@
+// codigo
+//  Pedir al usuario que ingrese 3 números
+// Imprime en consola el mayor
+// Opcional- usar alert
+
+let numero1 = window.prompt('Dame un número', 'number')
+if(numero1 === null ) {
+    console.log('No hay numero')
+}
+
+let numero2 = window.prompt('Dame un segundo número', 'number')
+if(numero2 === null ) {
+    console.log('No hay numero')
+}
+
+let numero3 = window.prompt('Dame un tercer número', 'number')
+if(numero3 === null ) {
+    console.log('No hay numero')
+}
+console.log()
---
+++ ternario.js
@@ -0,0 +1,2 @@
+let firstnumber = parseInt(prompt('Dame un numero', 'number'))
+let secondnumber = parseInt(prompt('Dame otro numero', 'number'))
