a05bec041423e1be88c30196c435ac0bc853fb01
RodrigoLeondev <rodrigoleon.dev@gmail.com>
2022-05-19 22:11:57 -0500

    ADD class three

--- string_methods.js
+++ string_methods.js
@@ -6,3 +6,25 @@
 // .substring(start, end)
 // .include(searchEle,ent, formIndex)
 // .slice
+
+//Ejercicio
+
+// Hola koders   * cuantas vocales existen * * cuantos caracteres son *
+// * cortar el string en 2 string --> 'hola' 'koders'
+
+/*
+let text = 'Hola koders';
+const myArray = text.split(" ")
+*/
+
+let text = 'Hola koders';
+console.log(`La cadena ${text} tiene ${text.length} caracteres`);
+
+console.log(text.split(" "))
+
+console.log(text.replace(/[aeiouAEIOU]/g, "*"))
+
+console.log(text.match(/[aeiou]/g).length)
+
+
+
--- ternario.js
+++
@@ -1,8 +0,0 @@
-let firstnumber = parseInt(prompt('Dame un numero', 'number'))
-let secondnumber = parseInt(prompt('Dame otro numero', 'number'))
- 
-firstnumber > secondnumber === 0
-    ?
-    console.log(`El numero ${firstnumber} es mayor que ${secondnumber}`)
-    :
-    console.log
