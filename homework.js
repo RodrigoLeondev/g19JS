b8f9890a720fe378936af19588d5edf5b137f7b0
RodrigoLeondev <rodrigoleon.dev@gmail.com>
2022-05-24 19:43:27 -0500

    add homework

--- homework.js
+++ homework.js
@@ -12,13 +12,34 @@
 }
 
 */
+/*
 function temperatura (){
     
 let gradosCelsius = prompt('Vamos a convertir °C a °F', '¿A cuántos grados Celsius estás?')
 var convCF = (gradosCelsius * 9/5 +32) 
 alert('Estas a ${convCF}')
 
-}
+}*/
+
+ /*
+let numberOne
+let numberTwo
+let numberThree = parseInt(prompt('Numero 3'))
+
+console.log(addNumber (numberOne, numberTwo, numberThree))
+function test1(a, b = 0)
+console-log(a,b)
+
+        if( !a && !b ){
+            console.error('Error')
+        }
+        else {
+            return a + b
+        }
        
+*/
 
-
+for(i = 0; i <= 10; i++){
+    for(j = 0; j <= 10; j++)
+        console.log(`${i} x ${j} = ${ i * j}`)
+}
