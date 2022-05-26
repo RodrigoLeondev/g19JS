0b1fb5cc33dd87d19f0d0e6c43c0da5519cca89d
RodrigoLeondev <rodrigoleon.dev@gmail.com>
2022-05-25 23:50:38 -0500

    add class seven

--- array.js
+++ array.js
@@ -4,7 +4,7 @@
 // let newArray = [1.2.4]
 // let newArray = ['str', str2]
 // let newArray = [ [9.8.7], [9,8]]
-
+/*
 let newArray = [1,2,3, 'str', [1,2] ]
     console.log(newArray)
     //.lenght --> tamaño de array  --> cantidad de elementos que contiene
@@ -20,6 +20,24 @@ let newArray = [1,2,3, 'str', [1,2] ]
         arrReversed.push( arrayToReverse[i])
     }
         console.log(arrayToReverse, arrReversed)
+        arrReversed.push(3)
+        arr
     // Métodos de array 
     // .push() --> agregar nuevo item al array
     // .pop() --> elimina nuevo item al array
+*/
+
+// .map()
+/*
+let mapArr = [1,2,3,3]
+let doubleArr = mapArr.map( (cv,index, arr => {
+    let transformado = cv * 2
+    return transformado
+}))
+*/
+
+
+const getFirstLast = ('hola', 'mundo')
+let ultimas = getFirstLast.substring(-2)
+
+console.log(getFirstLast)
