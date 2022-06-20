beb66a3def077ec1d1e388a1d068b2a1b9f1d8ea
RodrigoLeondev <rodrigoleon.dev@gmail.com>
2022-06-19 21:05:12 -0500

    add tips and excercises of the class

--- bom.js
+++ bom.js
@@ -1,3 +1,18 @@
+// DOM
+// Document Object Model --> como manipula el doc html
+// document.querySelector('item')
+// element.classlist.add('clase nueva')
+// element.innerHTML = 'nuevohtml' --> modifica todo el html de un elemento
+// elment.addAttribute('propiedad', 'valor') --> agrega nuevo atributo () --> es un método
+// document.getElementsByTagName('h2') [1]
+// document.createElement('h2')
+// document.getElementById('idDelInput').value --> se obtiene un valor de un input
+
+// element.addEventListener('click', callback) --> agrega un Listener al evento click
+// callback es una funcion basada como parámetro, click es el evento = si hay un click, se ejecuta la función
+// element.addEventListener('click', () => { ... } )
+ 
+
 // BOM
-// Browser Object Model
+// Browser Object Model 
 // Comunicarse con el navegador
