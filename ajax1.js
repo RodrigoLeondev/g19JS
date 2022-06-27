16c026693d15ca8de1b1b4bdb898e66be1a99f6f
RodrigoLeondev <rodrigoleon.dev@gmail.com>
2022-06-26 23:45:49 -0500

    add exercise

--- ajax1.js
+++ ajax1.js
@@ -1,135 +1,108 @@
-/*     // JSON.parse( texto ) --> recibe un obj
-    // JSON.stringify( obj ) --> recibe un texto
 
-const createUser = (objUser) => {
-    console.log(JSON.stringify(objUser))
-}
+/**
+ * 
+ * 1. LLenar el endpoint de firebase con multiples posts
+ * 2. Crear una funcion que obtenga todos los posts y los imprima en una lista de cards
+ * Referencia:
+ * https://getbootstrap.com/docs/5.2/components/card/#content
+ * 
+ * 3. Crear un nuevo documento html con un formulario que tenga lo siguiente
+ * 3 campos (titulo, contenido, fecha )
+ * 1 botón de enviar
+ * Referecia:
+ * https://getbootstrap.com/docs/5.2/forms/form-control/
+ * 
+ *  Acciones
+ *  cuando se de click en el boton de enviar, 
+ *  crear un nuevo post en firebase
+ *  Opcional: Mostrar un alert con el aviso de que ya se creo el post
+ * 
+ * Referencia: 
+ * https://getbootstrap.com/docs/5.2/components/alerts/#content
+ */
 
-const xhttp = new XMLHttpRequest() // -->estructura de una petición
-console.log(xhttp)
-xhttp.open(
-    "POST",
-    "https://koders19gjs-default-rtdb.firebaseio.com/users/.json",true)
+// Estructura sugerida de archivos
+/**
+ * createuser.html
+ * js/createPost.js
+ * css/createpost.css
+ */
 
-xhttp.onload = function(data){
-    console.log(data)
-    if(data.target.status >= 200 && data.target.status <= 399){
-        console.log(data.target.response)
-    }
+// Esctructura del post
+// { title, body, date }
 
+// url de firebase
+//  * urlFirebase /posts/.json
 
+// como obtener los valores de los input, textarea, select ...
+// let title = document.querySelector('#elemento').value
+// let body = document.querySelector('#elemento').value
+// let date = document.querySelector('#elemento').value
 
-    xhttp.send( JSON.stringify(objUser) ) 
-}
-
-let user = {
-    name: 'Rodrigo Alejandro',
-    lasName: 'Leon',
-    age: 27
-}
-// createUser( user ) */
-
-// Update
-// PATCH - actualiza un fragmento
-// PUT - actuliza todo
-
-const createUser =  ( objUser ) => {
-    console.log(JSON.stringify(objUser))
-    const xhttp = new XMLHttpRequest()
-    xhttp.open( "POST",  
-      "https://https://rodrigokoderjs19-default-rtdb.firebaseio.com/users/.json", true)
-    xhttp.onload = function(data) {
-      console.log(data)
-        if(data.target.status >= 200 && data.target.status <= 399){
-            console.log(data.target.response)
-        }
-    }
-    xhttp.send( JSON.stringify(objUser) )
-  }
-  
-  let user =  {
-    name: 'Rodrigo',
-    lastName: 'Leon',
-    age: 28
-}
-//     let user2 =  {
-
-//     name: 'Paola',
-//     lastName: 'Zamarripa',
-//     age: 28
-// }
-//     let user3 =  {
-//     name: 'Adalberto',
-//     lastName: 'Zamarripa',
-//     age: 47
-// }
-//     let user4 =  {
-//     name: 'Elisa',
-//     lastName: 'Juarez',
-//     age: 47
-// }
-//     let user5 =  {
-//     name: 'Natalia',
-//     lastName: 'Zaam',
-//     age: 18
-// }
-//     let user6 =  {
-//     name: 'Osvaldo',
-//     lastName: 'Martinez',
-//     age: 42
-// }
-//     let user7 =  {
-//     name: 'Chris',
-//     lastName: 'Rocha',
-//     age: 41
-// }
-//     let user8 =  {
-//     name: 'Guillermo',
-//     lastName: 'Chigo',
-//     age: 36
-// }
-//     let user9 =  {
-//     name: 'Israel',
-//     lastName: 'Acevedo',
-//     age: 44
-// }
-//     let user10 =  {
-//     name: 'Jesus',
-//     lastName: 'Silva',
-//     age: 28
-// }
-  //createUser( user )
- //
- //
- const updateUser = ( newUser ) => {
-    const updatexhr = new XMLHttpRequest()
-    updatexhr.open('PATCH', 'https://rodrigokoderjs19-default-rtdb.firebaseio.com/users.json', true)
-
-    updatexhr.onload = (response) => {
-        if(response.target.status >= 200 && response.target.status <= 399){
-            console.log(response.target)
-            console.log(response.target.response)
-        }
-    }
-    updatexhr.send( JSON.stringify(userUpdated))
-
-  }
-
-
-  const deleteUser  = ( idUser ) => {
-    const deletexhr = new XMLHttpRequest()
-    deletexhr.open(
-      'DELETE', 
-      `https://koders19gjs-default-rtdb.firebaseio.com/users/${idUser}.json`, 
+const ajaxXHR = (callback, url, method = 'GET', obj = {} ) => {
+  const xhttp = new XMLHttpRequest()
+  xhttp.open( 
+      method,  
+      `https://rodrigokoderjs19-default-rtdb.firebaseio.com/POST/.json`, 
       true
-    )
-    deletexhr.onload = (response) => {
-      if(response.target.status >= 200 && response.target.status <= 399){
-        console.log(response.target)
-        console.log(response.target.response)
+  )
+  xhttp.onload = function(data) {
+      if(data.target.status >= 200 && data.target.status <= 399){
+          let response = JSON.parse(data.target.response)
+          callback(response)
       }
-    }
-    deletexhr.send()
   }
+  if(method === 'GET' || method === 'DELETE'){
+      xhttp.send()
+  } else {
+      xhttp.send( JSON.stringify(obj) )
+  }}
+
+
+
+
+const postCreated = (resp) => {
+  // { 
+  //     name:'-id' 
+  // }
+  if(resp.name !== ''){
+      document.querySelector('#wrap__alert').classList.remove('d-none')
+      document.querySelector('#id__post').innerHTML = resp.name
+
+      // timer
+      setTimeout( () => {
+          document.querySelector('#wrap__alert').classList.add('d-none')
+          document.querySelector('#id__post').innerHTML = ''
+      }, 4000)
+      
+      // clear form
+      document.querySelector('#title').value = ''
+      document.querySelector('#body').value = ''
+      document.querySelector('#datepost').value = '2022-06-10'
+  }
+}
+
+let btnEnviar = document.querySelector('#create__post')
+btnEnviar.addEventListener('click', () => {
   
-  deleteUser( '-N4AIvnfNeWo3gt8vvgY') 
+  let title = document.querySelector('#title').value
+  let body = document.querySelector('#body').value
+  let datepost = document.querySelector('#datepost').value
+
+  if( title !== '' && body !== '' && datepost !== '' ){
+
+      let newPost = {
+          title: title,
+          body: body,
+          date: datepost
+      }
+
+      console.log(newPost)
+      
+      ajaxXHR(postCreated, '/posts/.json', 'POST', newPost)
+
+  } else {
+      alert('Algunos datos estan vacios')
+  }
+})
+
---
+++ createPostHome.js
@@ -0,0 +1,71 @@
+
+const ajaxXHR = (callback, url, method = 'GET', obj = {} ) => {
+    const xhttp = new XMLHttpRequest()
+    xhttp.open( 
+        method,  
+        `https://rodrigokoderjs19-default-rtdb.firebaseio.com/POST/.json`, 
+        true
+    )
+    xhttp.onload = function(data) {
+        if(data.target.status >= 200 && data.target.status <= 399){
+            let response = JSON.parse(data.target.response)
+            callback(response)
+        }
+    }
+    if(method === 'GET' || method === 'DELETE'){
+        xhttp.send()
+    } else {
+        xhttp.send( JSON.stringify(obj) )
+    }
+  }
+
+
+// ajaxXHR(printAllPosts, '/posts/.json', 'GET')
+const postCreated = (resp) => {
+    // { 
+    //     name:'-id' 
+    // }
+    if(resp.name !== ''){
+        document.querySelector('#wrap__alert').classList.remove('d-none')
+        document.querySelector('#id__post').innerHTML = resp.name
+
+        // timer
+        setTimeout( () => {
+            document.querySelector('#wrap__alert').classList.add('d-none')
+            document.querySelector('#id__post').innerHTML = ''
+        }, 10000)
+        
+        // clear form
+        document.querySelector('#title').value = ''
+        document.querySelector('#body').value = ''
+        document.querySelector('#datepost').value = '2022-06-10'
+    }
+}
+
+let btnEnviar = document.querySelector('#create__post')
+btnEnviar.addEventListener('click',  () => {
+    
+    let title = document.querySelector('#title').value.trim()
+    let body = document.querySelector('#body').value.trim()
+    let datepost = document.querySelector('#datepost').value.trim()
+
+    if( title !== '' && body !== '' && datepost !== '' ){ // Validación de que no hay datos vacíos
+
+        let newPost = { // se hace el envío, con esto se forma el post
+            title: title,
+            body: body,
+            date: datepost
+        }
+
+        console.log(newPost)
+        
+
+        // Ejecuta ajaxXHR, cuando este todo listo, manda a llamar
+        // postCreated, luego la envías a esa url a la que se hará un POST 
+        // y luego mandas el nuevo objeto creado newPost
+        ajaxXHR(postCreated, '/posts/.json', 'POST', newPost)
+
+    } else {
+        alert('Algunos datos estan vacios') // si estan vacío se manda la alerta
+    }
+})
